2db02890d96e75c93f59ec8171c72e3cd0f5dbf3
Dmitriy Dementiy <d.dementiy@gmail.com>
2019-05-02 18:00:39 +0300

    Version 1.2.0
    Made brain-even game

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ const sayHi = () => {
   console.log('Welcome to the Brain Games!');
   const getName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${getName}!`);
+  console.log('Type \'brain-even\' to play Brain Even game.');
 };
 export default sayHi;
