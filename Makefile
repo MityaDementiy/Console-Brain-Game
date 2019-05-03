6ee611ae4f108fb00276d03f830692f95afe1406
Dmitriy Dementiy <d.dementiy@gmail.com>
2019-05-03 20:54:10 +0300

    Minor changes
    Minor changes

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ even:
 	npx babel-node -- src/bin/braineven.js
 
 calc: 
-    npx babel-node -- src/bin/brain-calc.js
+    npx babel-node -- src/bin/braincalc.js
 	
 publish: 
 	npm publish
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import readlineSync from 'readline-sync';
 
 const sayHi = () => {
   console.log('Welcome to the Brain Games!');
-  const getName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${getName}!`);
   console.log('Type \'brain-even\' to play Brain Even game.');
   console.log('Type \'brain-calc\' to play Brain Calc game.');
 };
