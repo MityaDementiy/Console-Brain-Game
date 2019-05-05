0f55308ce84f7e47a6ed7f3cd621ae757e1d3dea
Dmitriy Dementiy <d.dementiy@gmail.com>
2019-05-05 12:37:22 +0300

    Minor bug fixed
    Minor bug fixed

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -3,4 +3,5 @@
 console.log('Welcome to the Brain Games!');
 console.log('Type brain-even to play Brain Even round!');
 console.log('Type brain-calc to play Brain Calc Round!');
+console.log('Type brain-gsd to play Brain Find gratest divisor Round!');
 console.log('Good Luck!');
